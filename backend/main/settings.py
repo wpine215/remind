e4862ce4401a3cf0049cca64df3a8a180446e984
Will <wpine215@gmail.com>
2019-11-16 16:10:29 -0500

    Updated backend

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',,
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
